03dcc6b41ba8e9d0235be5565d856657469e813e
AmaoLearning <darksoul1remaked@outlook.com>
2025-12-01 13:19:41 +0800

    train and test different loss function

--- test_cross_datasets.sh
+++ test_cross_datasets.sh
@@ -1,1 +1,5 @@
-CUDA_VISIBLE_DEVICES=7 python test_IQA.py --model_path ./checkpoints --model_name hyperIQA_baseline --datasets koniq-10k spaq kadid agiqa
+CUDA_VISIBLE_DEVICES=6 python test_IQA.py --model_path ./checkpoints --model_name hyperIQA_residual_v2LossTest_l2 --datasets koniq-10k spaq kadid agiqa
+CUDA_VISIBLE_DEVICES=7 python test_IQA.py --model_path ./checkpoints --model_name hyperIQA_residual_v2Loss_srcc --datasets koniq-10k spaq kadid agiqa
+CUDA_VISIBLE_DEVICES=0 python test_IQA.py --model_path ./checkpoints --model_name hyperIQA_residual_v2Loss_plcc --datasets koniq-10k spaq kadid agiqa
+CUDA_VISIBLE_DEVICES=2 python test_IQA.py --model_path ./checkpoints --model_name hyperIQA_residual_v2Loss_rank --datasets koniq-10k spaq kadid agiqa
+CUDA_VISIBLE_DEVICES=5 python test_IQA.py --model_path ./checkpoints --model_name hyperIQA_residual_v2Loss_pairwise --datasets koniq-10k spaq kadid agiqa
--- train_koniq-10k.sh
+++ train_koniq-10k.sh
@@ -2,12 +2,12 @@
 #CUDA_VISIBLE_DEVICES=6 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2 --model_type residual --test_batch_size 96
 
 # test one train and test process
-CUDA_VISIBLE_DEVICES=6 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2LossTest --model_type residual --epochs 1 --train_test_num 1 --test_batch_size 96 --loss_type l1
-CUDA_VISIBLE_DEVICES=6 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2LossTest --model_type residual --epochs 1 --train_test_num 1 --test_batch_size 96 --loss_type l2
-CUDA_VISIBLE_DEVICES=6 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2LossTest --model_type residual --epochs 1 --train_test_num 1 --test_batch_size 96 --loss_type srcc
-CUDA_VISIBLE_DEVICES=6 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2LossTest --model_type residual --epochs 1 --train_test_num 1 --test_batch_size 96 --loss_type plcc
-CUDA_VISIBLE_DEVICES=6 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2LossTest --model_type residual --epochs 1 --train_test_num 1 --test_batch_size 96 --loss_type rank
-CUDA_VISIBLE_DEVICES=6 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2LossTest --model_type residual --epochs 1 --train_test_num 1 --test_batch_size 96 --loss_type pairwise
+#CUDA_VISIBLE_DEVICES=6 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2LossTest --model_type residual --epochs 1 --train_test_num 1 --test_batch_size 96 --loss_type l1
+CUDA_VISIBLE_DEVICES=6 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2Loss --model_type residual --test_batch_size 96 --loss_type l2
+CUDA_VISIBLE_DEVICES=7 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2Loss --model_type residual --test_batch_size 96 --loss_type srcc
+CUDA_VISIBLE_DEVICES=0 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2Loss --model_type residual --test_batch_size 96 --loss_type plcc
+CUDA_VISIBLE_DEVICES=2 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2Loss --model_type residual --test_batch_size 96 --loss_type rank
+CUDA_VISIBLE_DEVICES=5 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_residual_v2Loss --model_type residual --test_batch_size 96 --loss_type pairwise
 
 # test simple epoch running time
 #CUDA_VISIBLE_DEVICES=6 python train_test_IQA.py --dataset koniq-10k --model_name hyperIQA_baseline_ac --model_type baseline --epochs 1 --train_test_num 1 --test_batch_size 96
